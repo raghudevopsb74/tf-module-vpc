ae6456fdf8b04bee681d93cf303c8391f7f94c8f
Raghu K <raghuk.vit@gmail.com>
2023-08-18 07:07:27 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -42,9 +42,21 @@ resource "aws_route" "ngw" {
   nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
 }
 
-
 resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id = aws_vpc.main.id
   vpc_id      = var.default_vpc_id
   auto_accept = true
+}
+
+resource "aws_route" "peer" {
+  count                     = length(local.private_route_table_ids)
+  route_table_id            = element(local.private_route_table_ids, count.index)
+  destination_cidr_block    = var.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
+
+resource "aws_route" "default-vpc-peer-entry" {
+  route_table_id            = var.default_vpc_route_table_id
+  destination_cidr_block    = var.cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
