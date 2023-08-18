98726aa3c786f98252b23721a3887482afc9a19e
Raghu K <raghuk.vit@gmail.com>
2023-08-18 07:10:49 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -60,3 +60,8 @@ resource "aws_route" "default-vpc-peer-entry" {
   destination_cidr_block    = var.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
+
+resource "aws_instance" "main" {
+  instance_type = "t3.micro"
+  image_id = "ami-03265a0778a880afb"
+}
