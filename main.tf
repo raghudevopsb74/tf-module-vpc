7f19565194ac9e75cc8f73962c9da6ed99b7d3d1
Raghu K <raghuk.vit@gmail.com>
2023-08-18 06:34:22 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -24,6 +24,17 @@ resource "aws_route" "igw" {
   gateway_id             = aws_internet_gateway.igw.id
 }
 
+resource "aws_eip" "ngw" {
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  for_each      = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  allocation_id = lookup(aws_eip, each.value["id"], null)
+  subnet_id     = each.value["id"]
+}
+
 output "subnet" {
   value = module.subnets
 }
